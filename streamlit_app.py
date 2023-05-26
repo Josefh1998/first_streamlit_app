dd6fb4e0e79224f4af7068a9bbe58a4d55fd9230
Josefh1998 <134666150+Josefh1998@users.noreply.github.com>
2023-05-26 10:48:21 +0800

    Update streamlit_app.py
    import pandas library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 #created the new python file
 
+import pandas
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
@@ -9,3 +10,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥗 Tapsilog')
 streamlit.text('🥗 Hotsilog')
 streamlit.text('🥗 Longsilog')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
