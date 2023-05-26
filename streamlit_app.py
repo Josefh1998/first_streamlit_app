95289a55367a2f273d9d1df938e8d86b18ca9dd0
Josefh1998 <134666150+Josefh1998@users.noreply.github.com>
2023-05-26 10:44:24 +0800

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Tapsilog')
+streamlit.text('Hotsilog')
+streamlit.text('Longsilog')
