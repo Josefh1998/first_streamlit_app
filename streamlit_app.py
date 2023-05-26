e2885be69ad6c20e70eb4bdfd169b14523f6fa84
Josefh1998 <134666150+Josefh1998@users.noreply.github.com>
2023-05-26 14:20:59 +0800

    Update streamlit_app.py
    asdasdasd

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import streamlit
 import requests
 import snowflake.connector
 
+
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
@@ -12,14 +14,18 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("FOOD LIST")
 streamlit.dataframe(my_data_rows)
 
+fruit_choices = streamlit.text_input('What fruit would you like information about?')
+streamlit.write('The user entered ', fruit_choices)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choices)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+# fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+# streamlit.write('The user entered ', fruit_choice)
+
+# streamlit.header("Fruityvice Fruit Advice!")
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# streamlit.dataframe(fruityvice_normalized)
 
 # my_fruit_list = my_fruit_list.set_index('Fruit')
 # # Let's put a pick list here so they can pick the fruit they want to include 
